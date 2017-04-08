e8840c6d64726e44cd819e7ef605c1afddf7359a
Patricio López <lopezjuripatricio@gmail.com>
2017-04-08 12:44:34 -0300

    Setup dev eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 2017,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true,
+    },
+  },
+  "env": {
+    "es6": true,
+    "jest": true,
+    "browser": true,
+    "node": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "prettier",
+    "prettier/react",
+  ],
+  "plugins": [
+    "react",
+    "prettier",
+  ],
+  "rules": {
+    "prettier/prettier": ["error", {
+      "trailingComma": "es5",
+    }]
+  }
+};
---
+++ packages/react-resume/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Core extends React.Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default Core;
